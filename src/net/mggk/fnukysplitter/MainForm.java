87c009017ffd9e4bcc530eeff72c6dd445bca84e
mGGk <arino.guillaume@gmail.com>
2018-06-16 10:44:43 +0200

    Extracting part is done

--- src/net/mggk/fnukysplitter/MainForm.java
+++ src/net/mggk/fnukysplitter/MainForm.java
@@ -4,7 +4,10 @@ import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainForm {
 
@@ -17,6 +20,7 @@ public class MainForm {
     private JButton btnStartSplitter;
     private JLabel lblSpoof;
     private JTextField tfSpoof;
+    private JTextArea taLogs;
 
     //App entry point
     public static void main(String[] args){
@@ -75,9 +79,52 @@ public class MainForm {
                     JOptionPane.showMessageDialog(null, "The selected file doesn't exist !");
                     return;
                 }
-                //If you're getting here, well everything is ready for splitting, we're settings tool directory
-                String toolPath = System.getProperty("user.dir").concat("\\tools");
+                btnStartSplitter.setEnabled(false);
+                extractXCI();
             }
         });
+    }
+
+    public void extractXCI(){
+        SwingWorker sw = new SwingWorker(){
+            protected Object doInBackground() throws Exception {
+                //Getting file info
+                File xciFile = new File(tfFileDir.getText());
+                //If you're getting here, well everything is ready for splitting, we're settings tool directory
+                String toolPath = System.getProperty("user.dir").concat("\\tools");
+                //Generating first hactool command to extract game
+                ProcessBuilder hacTool = new ProcessBuilder(
+                        "cmd.exe", "/c", "cd \""+toolPath+"\" && hactool.exe -k \""+toolPath+"\\keys.txt\" -t xci \""+xciFile.getAbsolutePath()+"\" --outdir=\""+toolPath+"\\out\"");
+                hacTool.redirectErrorStream(true);
+                //Creating process and execute it
+                Process p = null;
+                try {
+                    p = hacTool.start();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+                //Reading output and put this in the log textarea
+                BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                String line = null;
+                while (true) {
+                    try {
+                        line = r.readLine();
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                    if (line == null) { break; }
+                    taLogs.append(line.concat(System.getProperty("line.separator")));
+                }
+                return null;
+            }
+            public void done(){
+                if(SwingUtilities.isEventDispatchThread()){
+                    btnStartSplitter.setEnabled(true);
+                    JOptionPane.showMessageDialog(null, "Game Ready :D");
+                }
+
+            }
+        };
+        sw.execute();
     }
 }
